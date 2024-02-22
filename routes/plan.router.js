2e63d344b8adfbfafe08738b4d99ed05a00ebd49
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2024-02-22 22:44:17 +0500

    user login str

--- routes/plan.router.js
+++ routes/plan.router.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { subscribe } = require("../controllers/plan.controller");
 const { subscribeValidator } = require("../middlewares/plan.middleware");
+const { isLogedin } = require("../middlewares/user.middleware");
 
 // router.route("/").get().post();
 
+router.use(isLogedin);
 router.post("/subscribe", subscribeValidator, subscribe);
 
 module.exports = router;
